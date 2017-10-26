0a1c7f88d6f6d1b927f292ba609601ce272c8277
WangJinzhong <andywang7259@163.com>
2017-10-26 18:38:27 +0800

    fix

--- lib/rails_sprite/stylesheet_generator.rb
+++ lib/rails_sprite/stylesheet_generator.rb
@@ -53,7 +53,8 @@ module RailsSprite
       styles << <<-END_CSS
 // 将下面3行，放入页面中(仅供参考)
 // .#{result[:css_class_shared]} {
-//   background: url(<%= static_url("#{result[:image_scope_name]}") %>) no-repeat;
+//   background-image: url(<%= image_path("#{result[:image_scope_name]}") %>) no-repeat;
+//   background-repeat: no-repeat;
 // }
 END_CSS
 
@@ -73,7 +74,8 @@ END_CSS
 
       styles << <<-END_CSS
 .#{result[:css_class_shared]} {
-  background: url(<%= image_path("#{result[:image_scope_name]}") %>) no-repeat;
+  background-image: url(<%= image_path("#{result[:image_scope_name]}") %>) no-repeat;
+  background-repeat: no-repeat;
 }
 END_CSS
 
